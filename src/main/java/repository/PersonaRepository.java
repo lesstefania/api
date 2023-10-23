c9791101a4922c0c12b110b8d919d814d4c601ce
Julio Cesar Robles Cazares <icsyulio@gmail.com>
2023-10-22 23:57:15 -0600

    carga inicial

---
+++ src/main/java/repository/PersonaRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+	import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import entity.Persona;
+@Repository
+	public interface PersonaRepository extends JpaRepository<Persona, Long> {
+		
+		Persona getById(int id);
+		
+
+	    //
+	}
+
